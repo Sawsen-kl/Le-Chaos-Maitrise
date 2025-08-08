1b02ea7345a224f676746fa6eb8c8d570c296a6f
Anomaly2019 <ibn.tinkert@protonmail.com>
2025-08-08 16:26:47 +0100

    Create conversion.py
    FEAT: Ajout de fonctions de conversion

---
+++ conversion.py
@@ -0,0 +1,11 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def km_to_miles(km):
+    return km * 0.621371
+
+def miles_to_km(miles):
+    return miles / 0.621371
